ef4125cc025d32d49fd274415eb2c0d76fdbb6e4
ParaQueNome <guiagusto455@gmail.com>
2023-04-25 17:38:16 -0300

    Alterando a conexao do banco de dados para a classe Pessoa no metodo construtor

--- Classes/Pessoa.php
+++ Classes/Pessoa.php
@@ -2,8 +2,16 @@
 
 abstract class Pessoa{
 
-    function 
+    function _construct(){
+        $this->connectDatabase();
+    }
+    function connectDatabase($nome, $host, $user, $database){
+        $mysqli = new mysqli($hostname, $user, $pass, $database);
+        if($mysqli->connect_errno){
+            die("Houve um erro");
+    }
 
+    }
 
 }
 
