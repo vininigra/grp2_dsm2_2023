e584b7cd9a0a50f487472034071eba0e2a48fe72
ParaQueNome <guiaugusto455@gmail.com>
2023-05-02 11:52:35 -0300

    Adicionando comentarios no metodo cadastrar

--- base_site/template_seron/Classes/Participante.php
+++ base_site/template_seron/Classes/Participante.php
@@ -1,7 +1,7 @@
 <?php
 require_once('Pessoa.php');
 require_once('Conexao.php');
-require_once('dados.php');
+
 
 class Participante extends Pessoa {
     private $idade, $senha;
@@ -17,11 +17,15 @@ class Participante extends Pessoa {
         $senha = mysqli_real_escape_string($this->connect->getConnection(), $senha);
         $nome = mysqli_real_escape_string($this->connect->getConnection(), $nome);
         $idade = mysqli_real_escape_string($this->connect->getConnection(), $idade);
-        
+        // Selecao dos dados para checagem se o email que foi inserido já consta no banco de dados
         $sql = "SELECT email FROM participante WHERE email = '$email'";
+        // Chamada do metodo da classe Conexao, getConnect que retorna o objeto conexao criado via MYSQLI
         $result = $this->connect->getConnection()->query($sql);
+        // Verificando se o e-mail já está cadastrado
+        // Se retornar alguma informacao pelo resultado, o email constará cadastrado
         if ($result->num_rows > 0) {
             echo "Email já cadastrado";
+        // Se retornar 0, a condição vai chamar uma query INSERT para colocar as informacoes do novo cadastro no banco de dados
         } else {
             $insert = "INSERT INTO participante(nome, email, senha, idade) VALUES('$nome', '$email', '$senha', '$idade')";
             if ($this->connect->getConnection()->query($insert) === TRUE) {
