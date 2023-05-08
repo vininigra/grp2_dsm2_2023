fb183bef8a64b8c4079445a6731a8d0198e9013b
ParaQueNome <guiaugusto455@gmail.com>
2023-05-07 21:57:01 -0300

    Merge nas branches

--- base_site/template_seron/Classes/Participante.php
+++ base_site/template_seron/Classes/Participante.php
@@ -16,12 +16,7 @@ class Participante extends Pessoa {
         $senha = mysqli_real_escape_string($this->connect->getConnection(), $senha);
         $nome = mysqli_real_escape_string($this->connect->getConnection(), $nome);
         $idade = mysqli_real_escape_string($this->connect->getConnection(), $idade);
-<<<<<<< HEAD
-    }
-    private function insercao($nome, $idade, $email, $senha){
-=======
-        // Selecao dos dados para checagem se o email que foi inserido já consta no banco de dados
->>>>>>> a9193ade63a2f3622100401ad3e650dd75f0b3f0
+
         $sql = "SELECT email FROM participante WHERE email = '$email'";
         // Chamada do metodo da classe Conexao, getConnect que retorna o objeto conexao criado via MYSQLI
         $result = $this->connect->getConnection()->query($sql);
@@ -29,13 +24,7 @@ class Participante extends Pessoa {
         // Se retornar alguma informacao pelo resultado, o email constará cadastrado
         if($result->num_rows > 0) {
             echo "Email já cadastrado";
-<<<<<<< HEAD
-            
-        } else {
-=======
-        // Se retornar 0, a condição vai chamar uma query INSERT para colocar as informacoes do novo cadastro no banco de dados
-        }else{
->>>>>>> a9193ade63a2f3622100401ad3e650dd75f0b3f0
+
             $insert = "INSERT INTO participante(nome, email, senha, idade) VALUES('$nome', '$email', '$senha', '$idade')";
             if($this->connect->getConnection()->query($insert) === TRUE){
                 echo "Dados inseridos com sucesso";
--- base_site/template_seron/participante_cadastro.php
+++ base_site/template_seron/participante_cadastro.php
@@ -8,30 +8,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
     
     
-<<<<<<< HEAD
 
-    //Instancia o objeto participante já conectando ao banco de dados
-=======
-    // Verifica se o campo dt_nascimento foi preenchido e é uma data válida
-    if (empty($_POST["dt_nascimento"]) || !strtotime($_POST["dt_nascimento"])) {
-        echo "O campo data de nascimento é obrigatório e deve ser uma data válida";
-        exit;
-    }
-    
-    // Verifica se o campo senha foi preenchido
-    if (empty($_POST["senha"])) {
-        echo "O campo senha é obrigatório";
-        exit;
-    }
-    
-    if($_POST['senha'] != $_POST['confirmasenha']){
-        echo "Senhas não conferem";
-        exit;
-    }
-        
-    
-    // Se todos os campos foram preenchidos corretamente, cadastra o participante
->>>>>>> a9193ade63a2f3622100401ad3e650dd75f0b3f0
     $participante = new Participante($servername, $username, $password, $dbname);
     //Declarando as variaveis recebidas via POST
     $nome = $_POST['name'];
