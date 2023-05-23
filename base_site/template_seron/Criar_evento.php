7db2eeb7b24e83509ea096b3097c7a877e39e9e7
Vinicius Augusto Nigra <111812339+vininigra@users.noreply.github.com>
2023-05-23 19:52:46 -0300

    CRUD - Evento

---
+++ base_site/template_seron/Criar_evento.php
@@ -0,0 +1,160 @@
+<?php
+include_once "Classes/Conexao_evento.php";
+include_once "cria_evento.php";
+include_once "coloborador_evento.php";
+
+//instancia as classes
+$evento = new Evento();
+$eventodao = new EventoDAO();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Criar Evento</title>
+    <style>
+        .menu,
+        thead {
+            background-color: #bbb !important;
+        }
+
+        .row {
+            padding: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <nav class="navbar navbar-light bg-light menu">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                CRUD PHP POO
+            </a>
+        </div>
+    </nav>
+    <div class="container">
+        <form action="app/controller/UsuarioController.php" method="POST">
+            <div class="row">
+                <div class="col-md-3">
+                    <label>Data</label>
+                    <input type="date" name="data" value="" autofocus class="form-control" require />
+                </div>
+                <div class="col-md-5">
+                    <label>Hora</label>
+                    <input type="time" name="hora" value="" class="form-control" require />
+                </div>
+                <div class="col-md-2">
+                    <label>Local</label>
+                    <input type="text" name="local" value="" class="form-control" require />
+                </div>
+                <div class="col-md-2">
+                    <label>Esporte</label>
+                    <input type="text" name="tipo_esporte" value="" class="form-control" require />
+                </div>
+                <div class="col-md-2">
+                    <label>Idade</label>
+                    <input type="number" name="faixa_etaria" value="" class="form-control" require />
+                </div>
+                <div class="col-md-2">    
+                    <br>
+                    <button class="btn btn-primary" type="submit" name="cadastrar">Cadastrar</button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="table-responsive">
+            <table class="table table-sm table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Data</th>
+                        <th>Hora</th>
+                        <th>Local</th>
+                        <th>Esporte</th>
+                        <th>Idade</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($eventodao->read() as $evento) : ?>
+                        <tr>
+                            <td><?= $evento->getId() ?></td>
+                            <td><?= $evento->getData() ?></td>
+                            <td><?= $evento->getHora() ?></td>
+                            <td><?= $evento->getLocal() ?></td>
+                            <td><?= $evento->getTipo_esporte()?></td>
+                            <td><?= $evento->getFaixa_etaria() ?></td>
+                            <td class="text-center">
+                                <button class="btn  btn-warning btn-sm" data-toggle="modal" data-target="#editar><?= $evento->getId() ?>">
+                                    Editar
+                                </button>
+                                <a href="EventoControl.php?del=<?= $evento->getId() ?>">
+                                <button class="btn  btn-danger btn-sm" type="button">Excluir</button>
+                                </a>
+                            </td>
+                        </tr>
+                        <!-- Modal -->
+                        <div class="modal fade" id="editar><?= $evento->getId() ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Editar</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="EventoControl.php" method="POST">
+                                            <div class="row">
+                                                <div class="col-md-5">
+                                                    <label>Data</label>
+                                                    <input type="date" name="data" value="<?= $evento->getData() ?>" class="form-control" require />
+                                                </div>
+                                                <div class="col-md-7">
+                                                    <label>Hora</label>
+                                                    <input type="time" name="hora" value="<?= $evento->getHora() ?>" class="form-control" require />
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-3">
+                                                    <label>Local</label>
+                                                    <input type="text" name="local" value="<?= $evento->getLocal() ?>" class="form-control" require />
+                                                </div>
+                                                <div class="col-md-3">
+                                                <label>Esporte</label>
+                                                    <input type="text" name="tipo_esporte" value="<?= $evento->getTipo_esporte() ?>" class="form-control" require />
+                                                </div>
+                                                <div class="col-md-3">
+                                                <label>Idade</label>
+                                                    <input type="number" name="faixa_etaria" value="<?= $evento->getFaixa_etaria() ?>" class="form-control" require />
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-2">
+                                                    <br>
+                                                    <input type="hidden" name="id" value="<?= $evento->getId() ?>" />
+                                                    <button class="btn btn-primary" type="submit" name="editar">Cadastrar</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ base_site/template_seron/evento.php
@@ -0,0 +1,63 @@
+<?php
+
+class Evento{
+    
+    private $id;
+    private $data;
+    private $hora;
+    private $local;
+    private $tipo_esporte;
+    private $faixa_etaria;
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getData() {
+        return $this->data;
+    }
+
+    function getHora() {
+        return $this->hora;
+    }
+
+    function getLocal() {
+        return $this->local;
+    }
+
+    function getTipo_esporte() {
+        return $this->tipo_esporte;
+    }
+
+    function getFaixa_etaria() {
+        return $this->faixa_etaria;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setData($data) {
+        $this->data = $data;
+    }
+
+    function setHora($hora) {
+        $this->hora = $hora;
+    }
+
+    function setLocal($local) {
+        $this->local = $local;
+    }
+
+    function setTipo_esporte($tipo_esporte) {
+        $this->tipo_esporte = $tipo_esporte;
+    }
+
+    function setFaixa_etaria($faixa_etaria) {
+        $this->faixa_etaria = $faixa_etaria;
+    }
+
+    
+}
+?>
+
