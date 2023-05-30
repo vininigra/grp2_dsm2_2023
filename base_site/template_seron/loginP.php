e1077c990dd9cbc2e923aa7b40d36037865d5bb3
ParaQueNome <guiaugusto455@gmail.com>
2023-05-29 21:25:01 -0300

    Adicionando label do formulario login

--- base_site/template_seron/loginP.php
+++ base_site/template_seron/loginP.php
@@ -9,31 +9,52 @@
     <title>Login participante</title>
 </head>
 <body>
-    <form method="post" action="">
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required>
-    
-        <label for="senha">Senha:</label>
-        <input type="password" name="senha" id="senha" required>
-    
-        <input type="submit" value="Entrar">
-    </form>
+<form method="POST" action="">
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" required>
+
+    <label for="senha">Senha:</label>
+    <input type="password" name="senha" id="senha" required>
+
+    <label for="tipo">Tipo de Usuário:</label>
+    <select name="tipo" id="tipo">
+        <option value="colaborador">Colaborador</option>
+        <option value="participante">Participante</option>
+    </select>
+
+    <input type="submit" value="Login">
+</form>
 </body>
 </html>
 <?php
+session_start();
+
 require_once('Classes/Participante.php');
+require_once('Classes/Colaborador.php');
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
     if(isset($_POST['email']) && isset($_POST['senha'])){
-        $pessoa = new Participante($servername, $username, $password, $dbname);
         $email = $_POST['email'];
         $senha = $_POST['senha'];
-        $resultado = $pessoa->login($email, $senha);
-        if($resultado == "Email ou senha incorretos"){
-            echo "<p style='color:red'>$resultado</p><br>";
+        $tipo = $_POST['tipo'];
+    
+        if ($tipo === 'colaborador') {
+            $colaborador = new Colaborador();
+            $mensagem = $colaborador->login($email, $senha);
+        } elseif ($tipo === 'participante') {
+            $participante = new Participante();
+            $mensagem = $participante->login($email, $senha);
+        } else {
+            $mensagem = "Tipo de usuário inválido.";
+        }
+    
+        if ($mensagem === "Login realizado com sucesso!") {
+            header('location:index.php');
+            exit;
+        } else {
+            echo $mensagem;
         }
     }
-
     unset($pessoa);
 }
 ?>
