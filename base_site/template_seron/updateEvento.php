b61aaa79c14dbda354da8e27bfac842d89cda76f
matheusoms <mathmatiasjr1@gmail.com>
2023-06-05 23:43:29 -0300

    atualizacao_listagem_eventos

--- base_site/template_seron/updateEvento.php
+++ base_site/template_seron/updateEvento.php
@@ -3,7 +3,7 @@ session_start();
 // Verifica se a sessão está iniciada
 if (isset($_SESSION['loggedin'])) {
      $status = "Logado";
-     $sessao_id = $_SESSION['id'];
+     $session_id = $_SESSION['id'];
 } else {
     header('location: index.php');
 }
