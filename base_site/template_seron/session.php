1b1eedca606478ae51d0922d3173f94175bd9ff3
Guilherme Afonso <86564645+ParaQueNome@users.noreply.github.com>
2023-06-09 11:50:34 -0300

    Add files via upload

--- base_site/template_seron/session.php
+++ base_site/template_seron/session.php
@@ -4,9 +4,19 @@ session_start();
 // Verifica se a sessão está iniciada
 if (isset($_SESSION['loggedin'])){
     $status = "Logado";
-    $colaborador = $_SESSION['colaborador'];
-    $session_id = $_SESSION['id'];
     $adm = $_SESSION['adm'];
+    if($adm){
+        $nome = $_SESSION['user'];
+        $colaborador = $_SESSION['colaborador'];
+        $session_id = $_SESSION['id'];
+    }else if($colaborador){
+        $colaborador = $_SESSION['colaborador'];
+        $session_id = $_SESSION['id'];
+    }else{
+        $colaborador = $_SESSION['colaborador'];
+    }
+   
+    
 } else {
     $status = "Cadastre-se";
     header('Location: login2.php');   
